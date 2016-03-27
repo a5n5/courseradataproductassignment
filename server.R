3bc9fc22e9b0b711c5522d3ba0286dc354fc5bcb
Amlan Nanda <amlannanda@gmail.com>
2016-03-27 13:42:25 -0400

    first commit

---
+++ server.R
@@ -0,0 +1,16 @@
+# This app calculates your Body Mass Index (BMI) from your height and weight
+# Place the ui.R and server.R codes in the same directory and run by using the command runApp()
+
+
+library(shiny)
+
+shinyServer(
+  function(input,output){
+    output$oweight<-renderPrint({input$weight})
+    output$oheight<-renderPrint({input$height})
+    #bmi<-reactive({
+    #  bmi<-(input$weight*703)/(input$height*input$height)
+  #  })
+    output$obmi<-renderPrint({(input$weight*703)/(input$height*input$height)})
+}
+)
---
+++ ui.R
@@ -0,0 +1,24 @@
+# This app calculates your Body Mass Index (BMI) from your height and weight
+# Place the ui.R and server.R codes in the same directory and run by using the command runApp()
+library(shiny)
+shinyUI(pageWithSidebar(
+  headerPanel("Coursera Assignment- BMI Calculator"),
+  sidebarPanel(
+    numericInput('weight', 'Enter weight in lbs',150, min=90,max=500,step=1),
+    numericInput('height', 'Enter height in inches',60, min=20,max=96,step=1),
+    submitButton('Submit')
+  ),
+  mainPanel(
+    h4('Your weight in pounds is'),
+    verbatimTextOutput("oweight"),
+    h4('Your height in inches is'),
+    verbatimTextOutput("oheight"),
+    h4('Your BMI is'),
+    verbatimTextOutput("obmi"),
+    h5('BMI- Weight Status'),
+    h6('Below 18.5: Underweight'),
+    h6('18.5-24.9: Normal'),
+    h6('25-29.9: Overweight'),
+    h6('30+: Obese')
+  )
+))
